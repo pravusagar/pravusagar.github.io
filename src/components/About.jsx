19e06a49074c703c2f923ff42b57a301d687d7db
pravusagar <pravusagar@gmail.com>
2022-09-17 21:22:04 +0530

    test 3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,6 @@ function About({ Data }) {
         <div className="three columns">
           <img className="profile-pic" src="images/profilepic.jpg" alt="" />
         </div>
-
         <div className="nine columns main-col">
           <h2>About Me</h2>
           <p>{Data.aboutme}</p>
